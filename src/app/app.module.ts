4f3ec7f95d1e0cf245ec839d0e9bfee21dab1ee6
Nicolas Bello Camilletti <nbellocamilletti@gmail.com>
2016-08-20 18:23:34 -0300

    Splitted the AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }  from './hero.service';
 
 @NgModule({
   imports: [
@@ -11,9 +13,13 @@ import { HeroDetailComponent } from './hero-detail.component';
   ],
   declarations: [
     AppComponent,
+    HeroesComponent,
     HeroDetailComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
-
-export class AppModule { }
+export class AppModule {
+}
